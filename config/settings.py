cc9c2a0d0c9ec4752a1dec4f282356334bcc190a
androcatz1 <narraj2381@gmail.com>
2026-06-21 00:46:13 +0800

    added config folder to store reuseable variables

---
+++ config/settings.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+load_dotenv()
+
+class Settings:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+
+    MAX_RESULTS = 50
+    REQUEST_TIMEOUT = 30
+
+    BASE_URL = "https://www.googleapis.com/youtube/v3"
+
+    START_DATE = datetime(2026, 6, 15)
+
+    THROTTLE = 0.5
+    MAX_QUOTA = 7000
+
+    CHANNEL_IDS = [
+        "UCpWvshQVx1d7BqCsPnVuNIw",
+        "UCsWp7U58TZM8uOYtRrAqWhg",
+        "UC2p8wkJVSjsMsRv0MjTikgA",
+    ]
