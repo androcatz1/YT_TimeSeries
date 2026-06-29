05e735a8665fae97826adf715d00b510c907b04a
androcatz1 <narraj2381@gmail.com>
2026-06-29 23:55:17 +0800

    added transformed time series table and schema

--- database/schema.sql
+++ database/schema.sql
@@ -17,3 +17,28 @@ CREATE TABLE IF NOT EXISTS video_metrics_time_series (
     fetched_at TIMESTAMP(0),
     duration TEXT
 );
+
+CREATE TABLE IF NOT EXISTS video_metrics_time_series_transformed (
+    id BIGSERIAL PRIMARY KEY,
+    video_id TEXT,
+    title TEXT,
+    description TEXT,
+    tags TEXT,
+    category_id TEXT,
+    channel_title TEXT,
+    published_at TIMESTAMP,
+    views BIGINT,
+    likes BIGINT,
+    comments_count BIGINT,
+    platform TEXT,
+    topic TEXT,
+    fetched_at TIMESTAMP(0),
+    duration TEXT,
+    published_hour INT,
+    published_day VARCHAR(9),
+    published_month VARCHAR(9),
+    duration_mins DOUBLE PRECISION,
+    engagement_rate DOUBLE PRECISION,
+    duration_bucket TEXT,
+    days_since_published INT
+);
